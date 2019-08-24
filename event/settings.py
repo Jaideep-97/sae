fdbee21b9bfcd51381964730a5ba3aa8b7a784f4
JAIDEEP DATTA <34120588+Jaideep-97@users.noreply.github.com>
2019-08-25 01:51:41 +0530

    changes made

--- event/settings.py
+++ event/settings.py
@@ -118,10 +118,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 CURRENT_PATH = os.path.abspath(os.path.dirname(__file__))
-
-STATIC_ROOT = 'static/'
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+#STATIC_ROOT = 'static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+#STATICFILES_DIRS = (
+#                    os.path.join(CURRENT_PATH, 'static'),
+#)
 STATICFILES_DIRS = (
-                    os.path.join(CURRENT_PATH, 'static'),
+    os.path.join(PROJECT_ROOT, 'static'),
 )
